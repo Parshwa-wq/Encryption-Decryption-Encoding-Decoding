2dead5a17b402fb9c147d22cee8f37c804d8ace2
Parshwa <pasupatel7296@gmail.com>
2025-02-03 23:13:28 +0530

    binary

---
+++ Encode/binary.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class binary
+{
+    static Scanner input=new Scanner(System.in);
+    static String plain;
+    public static void main(String s[])
+    {
+        System.out.print("Plaintext: ");
+        plain=input.nextLine();
+
+        String arr[]=new String[plain.length()];
+
+        System.out.print("Binar: ");
+        for(int i=0;i<plain.length();i++)
+        {
+            char c=plain.charAt(i);
+            int ascii=c;
+            String binary="";
+
+            while(ascii>0)
+            {
+                int rem=ascii%2;
+                binary=rem+binary;
+                ascii/=2;
+            }
+
+            while(binary.length()!=8)
+            {
+                binary="0"+binary;
+            }
+
+            arr[i]=binary;
+        }
+
+        for(String binary_arr:arr)
+        {
+            System.out.print(binary_arr);
+        }
+    }
+}
